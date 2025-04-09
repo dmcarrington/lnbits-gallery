61abe9129dc4c26268039c1ab736ce6aad112f77
David Carrington <dmcarrington@protonmail.com>
2025-04-09 13:37:22 +0100

    basic gallery without lnbits

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`,
+        search: "",
+      },
+    ],
+  },
+};
